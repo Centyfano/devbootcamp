7e64e970571d50f3ec2128c93a00cd1d2b1cfb5c
Centyfano <caongare@gmail.com>
2022-01-30 13:59:27 +0300

    check env

--- config/db.js
+++ config/db.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
+// require("dotenv").config({ path: "../config/config.env" }); //;
 
 /**
  * Connect to database
  */
 const connectDB = async () => {
+	console.log("url is " + process.env.MONGO_URI);
 	const conn = await mongoose.connect(process.env.MONGO_URI, {
 		useNewUrlParser: true,
 	});
